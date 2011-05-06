1ef7f4fb7dfc7b8c96a997f7f024297a827e874d
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-05-06 16:36:56 -0400

    Updated setup.py to make 'python setup.py register' work nicely

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ setup(name='PyAVM',
       description='Simple pure-python AVM meta-data parsing',
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
+      license='MIT',
       url='https://github.com/astrofrog/pyavm',
-      packages=['pyavm']
+      packages=['pyavm'],
+      provides=['pyavm'],
+      keywords=['Scientific/Engineering'],
+      long_description=open('README.md', 'rb').read(),
+      classifiers=[
+                   "Development Status :: 3 - Alpha",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: MIT License",
+                  ],
      )
