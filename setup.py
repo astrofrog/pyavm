1099c74f538126ee26736a7d226fbe489a584be3
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-05-08 17:56:57 -0400

    Updated version strings to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PyAVM',
-      version='0.1.0',
+      version='0.1.1',
       description='Simple pure-python AVM meta-data parsing',
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
