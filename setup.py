7f8ff20a453a61dca1978d3e793ca823309c872c
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-04-09 16:53:24 +0200

    Make setup.py script Python 3.x compatible

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from distutils.core import setup
 
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
 version = '0.1.4'
 
 setup(name='PyAVM',
@@ -14,11 +19,12 @@ setup(name='PyAVM',
       download_url='https://github.com/downloads/astrofrog/pyavm/PyAVM-%s.tar.gz' % version,
       packages=['pyavm'],
       provides=['pyavm'],
+      cmdclass={'build_py': build_py},
       keywords=['Scientific/Engineering'],
-      long_description=open('README.md', 'rb').read(),
+      long_description=open('README.md', 'r').read(),
       classifiers=[
-                   "Development Status :: 3 - Alpha",
-                   "Programming Language :: Python",
-                   "License :: OSI Approved :: MIT License",
-                  ],
-     )
+      "Development Status :: 3 - Alpha",
+      "Programming Language :: Python",
+      "License :: OSI Approved :: MIT License",
+      ],
+      )
