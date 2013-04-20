095af423d2ff973161ac24657b109c7ccfc00925
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-04-21 00:21:23 +0200

    Fix bug with parsing certain JPEG files

--- pyavm/jpeg.py
+++ pyavm/jpeg.py
@@ -36,10 +36,10 @@ MARKERS[b'\xe9'] = 'APP9'
 MARKERS[b'\xfe'] = 'COM'
 MARKERS[b'\xd9'] = 'EOI'
 
-# Define which markers are followed by variable-length data
+# Define some markers which are always followed by variable-length data
 
-VARIABLE = ['SOF0', 'SOF2', 'DHT', 'DQT', 'SOS', 'APP0', 'APP1', 'APP2',
-            'APP3', 'APP4', 'APP5', 'APP6', 'APP7', 'APP8', 'APP9', 'COM']
+VARIABLE = ['APP0', 'APP1', 'APP2', 'APP3', 'APP4',
+            'APP5', 'APP6', 'APP7', 'APP8', 'APP9']
 
 
 def is_jpeg(filename):
@@ -79,12 +79,12 @@ class JPEGFile(object):
 
         self.segments = []
 
-        start = end = contents.find(b'\xff')
+        start = end = 0
         while True:
             if contents[start+1:start+2] in MARKERS and MARKERS[contents[start+1:start+2]] in VARIABLE:
-                end = contents.find(b'\xff', start + 4)
+                end = contents.find(b'\xff', end + 4)
             else:
-                end = contents.find(b'\xff', start + 1)
+                end = contents.find(b'\xff', end + 1)
             if end == -1 or contents[end + 1:end + 2] not in [b'\xff', b'\x00']:
                 if end == -1:
                     end = len(contents) + 1
