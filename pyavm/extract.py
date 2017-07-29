e582d169b3492bcef331c3e68f2ac11512275395
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-29 23:45:21 +0100

    Add warning if xmp_packet_index is not specified and there are multiple packets present

--- pyavm/extract.py
+++ pyavm/extract.py
@@ -1,14 +1,16 @@
 from __future__ import print_function, division
 
 import re
-import struct
 import warnings
 
-from .jpeg import is_jpeg, JPEGFile, JPEGSegment
-from .png import is_png, PNGFile, PNGChunk
+from .jpeg import is_jpeg, JPEGFile
+from .png import is_png, PNGFile
 from .exceptions import NoXMPPacketFound
 
-def extract_xmp(image, xmp_packet_index=0):
+__all__ = ['extract_xmp']
+
+
+def extract_xmp(image, xmp_packet_index=None):
 
     if is_jpeg(image):
 
@@ -24,6 +26,12 @@ def extract_xmp(image, xmp_packet_index=0):
                     xmp_segments.append(segment.bytes[32:])
 
         if len(xmp_segments) > 0:
+            if xmp_packet_index is None:
+                if len(xmp_segments) > 1:
+                    warnings.warn("Multiple XMP packets are present but "
+                                  "xmp_packet_index was not specified, assuming "
+                                  "xmp_packet_index=0")
+                xmp_packet_index = 0
             return xmp_segments[xmp_packet_index]
         else:  # No XMP data was found
             raise NoXMPPacketFound("No XMP packet present in file")
@@ -42,13 +50,20 @@ def extract_xmp(image, xmp_packet_index=0):
                     xmp_chunks.append(chunk.data[22:])
 
         if len(xmp_chunks) > 0:
+            if xmp_packet_index is None:
+                if len(xmp_chunks) > 1:
+                    warnings.warn("Multiple XMP packets are present but "
+                                  "xmp_packet_index was not specified, assuming "
+                                  "xmp_packet_index=0")
+                xmp_packet_index = 0
             return xmp_chunks[xmp_packet_index]
         else:  # No XMP data was found
             raise NoXMPPacketFound("No XMP packet present in file")
 
     else:
 
-        warnings.warn("Only PNG and JPEG files can be properly parsed - scanning file contents for XMP packet")
+        warnings.warn("Only PNG and JPEG files can be properly parsed "
+                      "- scanning file contents for XMP packet")
 
         with open(image, 'rb') as fileobj:
             contents = fileobj.read()
@@ -56,6 +71,12 @@ def extract_xmp(image, xmp_packet_index=0):
         start_positions = [m.start() for m in re.finditer(b"<?xpacket begin=", contents)]
 
         if len(start_positions) > 0:
+            if xmp_packet_index is None:
+                if len(start_positions) > 1:
+                    warnings.warn("Multiple XMP packets are present but "
+                                  "xmp_packet_index was not specified, assuming "
+                                  "xmp_packet_index=0")
+                xmp_packet_index = 0
             start = start_positions[xmp_packet_index] - 2
             end = contents.index(b"</x:xmpmeta>", start) + 12
         else:
